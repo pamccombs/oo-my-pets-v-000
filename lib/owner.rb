d3c0c25b1a40d357813357dd89d83a5586075462
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-06 20:13:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -45,4 +45,13 @@ class Owner
     pets[:dogs].each {|dog| dog.mood = "happy"}
   end
   
+  def play_with_cats
+    pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+  
+  def feed_fish
+    
+  end
+    
+  
 end
