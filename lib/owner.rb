00f8242ff7cdb7b92944e1794cbb8a4a614473ee
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-06 20:39:13 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,12 +53,12 @@ class Owner
     pets[:fishes].each {|fish| fish.mood = "happy"}
   end
   
-  def sell_pets
-    pets.each do |species, animals| 
-      animals.each do |animal|  animal.mood = "nervous"
-    end
-    animals.clear
-  end
+  #def sell_pets
+    #pets.each do |species, animals| 
+      #animals.each do |animal|  animal.mood = "nervous"
+    #end
+    #animals.clear
+  #end
   
   #def list_pets
      # "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
